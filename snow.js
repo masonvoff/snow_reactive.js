3752d1185ca227d29952c2e205ddbe870621490a
masonvoff <117608433+masonvoff@users.noreply.github.com>
2024-12-07 02:29:59 -0500

    Update snow.js
    reactive

--- snow.js
+++ snow.js
@@ -4,7 +4,7 @@
 */
 
 // Amount of Snowflakes
-var snowMax = 35;
+var snowMax = 50;
 
 // Snowflake Colours
 var snowColor = ["#DDD", "#EEE"];
@@ -13,7 +13,7 @@ var snowColor = ["#DDD", "#EEE"];
 var snowEntity = "&#x2022;";
 
 // Falling Velocity
-var snowSpeed = 0.75;
+var snowSpeed = 0.55;
 
 // Minimum Flake Size
 var snowMinSize = 8;
@@ -22,78 +22,104 @@ var snowMinSize = 8;
 var snowMaxSize = 24;
 
 // Refresh Rate (in milliseconds)
-var snowRefresh = 50;
+var snowRefresh = 25;
 
 // Additional Styles
 var snowStyles = "cursor: default; -webkit-user-select: none; -moz-user-select: none; -ms-user-select: none; -o-user-select: none; user-select: none;";
 
-/*
-// End of Configuration
-// ----------------------------------------
-// Do not modify the code below this line
-*/
+// Initialize mouse position and radius of repulsion
+var mouseX = -1;
+var mouseY = -1;
+var mouseRepulseRadius = 250;
 
+// Snowflakes array
 var snow = [],
-	pos = [],
-	coords = [],
-	lefr = [],
-	marginBottom,
-	marginRight;
+    pos = [],
+    coords = [],
+    lefr = [],
+    marginBottom,
+    marginRight;
 
+// Randomize function
 function randomise(range) {
-	rand = Math.floor(range * Math.random());
-	return rand;
+    return Math.floor(range * Math.random());
 }
 
+// Initialize snowflakes
 function initSnow() {
-	var snowSize = snowMaxSize - snowMinSize;
-	marginBottom = document.body.scrollHeight - 5;
-	marginRight = document.body.clientWidth - 15;
+    var snowSize = snowMaxSize - snowMinSize;
+    marginBottom = document.body.scrollHeight - 5;
+    marginRight = document.body.clientWidth - 15;
 
-	for (i = 0; i <= snowMax; i++) {
-		coords[i] = 0;
-		lefr[i] = Math.random() * 15;
-		pos[i] = 0.03 + Math.random() / 10;
-		snow[i] = document.getElementById("flake" + i);
-		snow[i].style.fontFamily = "inherit";
-		snow[i].size = randomise(snowSize) + snowMinSize;
-		snow[i].style.fontSize = snow[i].size + "px";
-		snow[i].style.color = snowColor[randomise(snowColor.length)];
-		snow[i].style.zIndex = 1000;
-		snow[i].sink = snowSpeed * snow[i].size / 5;
-		snow[i].posX = randomise(marginRight - snow[i].size);
-		snow[i].posY = randomise(2 * marginBottom - marginBottom - 2 * snow[i].size);
-		snow[i].style.left = snow[i].posX + "px";
-		snow[i].style.top = snow[i].posY + "px";
-	}
+    for (let i = 0; i <= snowMax; i++) {
+        coords[i] = 0;
+        lefr[i] = Math.random() * 15;
+        pos[i] = 0.03 + Math.random() / 10;
+        snow[i] = document.getElementById("flake" + i);
+        snow[i].style.fontFamily = "inherit";
+        snow[i].size = randomise(snowSize) + snowMinSize;
+        snow[i].style.fontSize = snow[i].size + "px";
+        snow[i].style.color = snowColor[randomise(snowColor.length)];
+        snow[i].style.zIndex = -1;
+        snow[i].sink = snowSpeed * snow[i].size / 5;
+        snow[i].posX = randomise(marginRight - snow[i].size);
+        snow[i].posY = randomise(2 * marginBottom - marginBottom - 2 * snow[i].size);
+        snow[i].style.left = snow[i].posX + "px";
+        snow[i].style.top = snow[i].posY + "px";
+    }
 
-	moveSnow();
+    moveSnow();
 }
 
+// Resize handler
 function resize() {
-	marginBottom = document.body.scrollHeight - 5;
-	marginRight = document.body.clientWidth - 15;
+    marginBottom = document.body.scrollHeight - 5;
+    marginRight = document.body.clientWidth - 15;
 }
 
+// Mousemove event to track cursor position
+document.addEventListener('mousemove', function(e) {
+    mouseX = e.pageX;
+    mouseY = e.pageY;
+});
+
+// Snowflake movement function
 function moveSnow() {
-	for (i = 0; i <= snowMax; i++) {
-		coords[i] += pos[i];
-		snow[i].posY += snow[i].sink;
-		snow[i].style.left = snow[i].posX + lefr[i] * Math.sin(coords[i]) + "px";
-		snow[i].style.top = snow[i].posY + "px";
+    for (let i = 0; i <= snowMax; i++) {
+        coords[i] += pos[i];
+        snow[i].posY += snow[i].sink;
 
-		if (snow[i].posY >= marginBottom - 2 * snow[i].size || parseInt(snow[i].style.left) > (marginRight - 3 * lefr[i])) {
-			snow[i].posX = randomise(marginRight - snow[i].size);
-			snow[i].posY = 0;
-		}
-	}
 
-	setTimeout("moveSnow()", snowRefresh);
+        var distanceX = snow[i].posX - mouseX;
+        var distanceY = snow[i].posY - mouseY;
+        var distance = Math.sqrt(distanceX * distanceX + distanceY * distanceY);
+
+        // Repel the snowflake when close to the mouse
+        if (distance < mouseRepulseRadius) {
+            var repelForce = (mouseRepulseRadius - distance) / mouseRepulseRadius;
+            snow[i].posX += (distanceX / distance) * repelForce * 10;
+            snow[i].posY += (distanceY / distance) * repelForce * 10;
+        }
+
+        // Apply normal motion and wrapping behavior
+        snow[i].style.left = snow[i].posX + lefr[i] * Math.sin(coords[i]) + "px";
+        snow[i].style.top = snow[i].posY + "px";
+
+        // Reset snowflake position when it falls off screen
+        if (snow[i].posY >= marginBottom - 2 * snow[i].size || parseInt(snow[i].style.left) > (marginRight - 3 * lefr[i])) {
+            snow[i].posX = randomise(marginRight - snow[i].size);
+            snow[i].posY = 0;
+        }
+    }
+
+    setTimeout(moveSnow, snowRefresh);
 }
 
-for (i = 0; i <= snowMax; i++) {
-	document.write("<span id='flake" + i + "' style='" + snowStyles + "position:absolute;top:-" + snowMaxSize + "'>" + snowEntity + "</span>");
+// Generate snowflakes
+for (let i = 0; i <= snowMax; i++) {
+    document.write("<span id='flake" + i + "' style='" + snowStyles + "position:absolute;top:-" + snowMaxSize + "'>" + snowEntity + "</span>");
 }
 
+// Event listeners
 window.addEventListener('resize', resize);
 window.addEventListener('load', initSnow);
